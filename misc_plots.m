8f10c6b4b56342d50bfd9e75d5489ed108f330fc
AaronvonMaekel <122726312+AaronvonMaekel@users.noreply.github.com>
2023-09-14 22:55:37 +0200

    Vergleichsbild eindimensionaler Systeme hinzugefügt

--- misc_plots.m
+++ misc_plots.m
@@ -157,4 +157,40 @@ ax = gca;
 ax.FontSize = fntsize;
 
 
+%% plotte alle drei funktionen
 
+xmesh=0:0.005:1;
+param_log = 3.9;
+param_tent = 1.95;
+param_bench = 2.2;
+ymesh=nan(3,length(xmesh))% log tent benchmark
+i=1
+for x=xmesh
+ymesh(1,i) = param_log*x*(1-x);
+ymesh(2,i) = param_tent*min([x 1-x]);
+ymesh(3,i) = (1-2^(1-param_bench))^-1 *(1 - x^param_bench- (1- x)^param_bench);
+i=i+1;
+end
+figure(12)
+
+plot(xmesh,ymesh(1,:),"b",LineWidth=1.5);
+hold on;
+plot(xmesh,ymesh(2,:),"g",LineWidth=1.5);
+hold on;
+plot(xmesh,ymesh(3,:),"r",LineWidth=1.5);
+hold on
+plot(xmesh,xmesh,"k");
+hold off;
+legend("logistische Gleichung","Zeltabbildung","Benchmark-System",location="south")
+%% how does the benchmark look for all theta?
+% figure(13)
+% for ii=2:0.1:3
+% i=1;
+% for x=xmesh
+% imesh(i) = (1-2^(1-ii))^-1 *(1 - x^ii- (1- x)^ii);
+% i=i+1;
+% end
+% plot(xmesh,imesh)
+% hold on;
+% end
+% plot(xmesh,xmesh)
